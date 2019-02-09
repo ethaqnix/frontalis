df718cc6e6074df5448c264740a1121d6e9732f4
Romain Denizot <romain.denizot@epitech.eu>
2019-02-09 14:31:39 +0100

    modif

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Button, withTheme } from 'react-native-elements';
 import { View } from 'react-native';
+import PropTypes from 'prop-types';
 
 const CustomButton = (props) => {
   const { theme } = props;
-  console.log(theme.Button);
   return (
     <View style={theme.Button}>
       <Button {...props} />
     </View>
   );
+};
+
+CustomButton.propTypes = {
+  theme: PropTypes.shape(),
+};
+
+CustomButton.defaultProps = {
+  theme: undefined,
 };
 
 export default withTheme(CustomButton);
