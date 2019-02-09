0ce251a57e211afc7e407e30193c4bc2791f3483
Maxime Parriaux <m.parriaux@carrevolutis.com>
2019-02-09 14:56:59 +0100

    Update proposal operator

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,10 @@ module.exports = function config(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-class-properties',
+    ],
     sourceMaps: true,
   };
 };
